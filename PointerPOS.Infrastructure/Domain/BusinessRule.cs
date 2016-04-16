af5f5ef0e863b356ff2bcf3e90858ea93b61c72a
anick-chy <anickchowdhury@gmail.com>
2016-04-17 03:48:15 +0600

    First Commit
    Entity Base, Value Object and Business Rule Classes are developed.

---
+++ PointerPOS.Infrastructure/Domain/BusinessRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointerPOS.Infrastructure.Domain
+{
+    public class BusinessRule
+    {
+        private string _ruleDescription;
+
+        public BusinessRule(string ruleDescription)
+        {
+            this._ruleDescription = ruleDescription;
+        }
+
+        public string RuleDescription
+        {
+            get
+            {
+                return _ruleDescription;
+            }
+        }
+    }
+}
---
+++ PointerPOS.Infrastructure/Domain/ValueObjectIsInvalidException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointerPOS.Infrastructure.Domain
+{
+    public class ValueObjectIsInvalidException : Exception
+    {
+        /// <summary>
+        /// Represent errors that occur if any value object is invalid
+        /// </summary>
+        public ValueObjectIsInvalidException(string message)
+            : base(message) { }
+    }
+}
