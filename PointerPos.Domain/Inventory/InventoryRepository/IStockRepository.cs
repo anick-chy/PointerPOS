4620e72add33422de82a183b3ddeaf1c9b780fef
anick-chy <anickchowdhury@gmail.com>
2016-06-06 02:45:37 +0600

    Stock Domain
    Stock Repository and concrete implementation skeleton.

---
+++ PointerPos.Domain/Inventory/InventoryRepository/IStockRepository.cs
@@ -0,0 +1,15 @@
+﻿using PointerPos.Domain.Inventory.Stock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointerPos.Domain.Inventory.InventoryRepository
+{
+    interface IStockRepository
+    {
+        float GetProductsQuantityInStock(int productId);
+        void AddProductToStock(int placeOfStockId, int productId, float quantity);
+        void DeductProductFromStock(int placeOfStockId, int productId, float quantity);
+    }
+}
---
+++ PointerPos.Domain/Inventory/Stock/StoreStock.cs
@@ -0,0 +1,33 @@
+﻿using PointerPos.Domain.Inventory.InventoryRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointerPos.Domain.Inventory.Stock
+{
+    public class StoreStock : IPlaceOfStock
+    {
+        private int StoreID;
+        private IStockRepository _stockRepository;
+        public StoreStock(int storeId)
+        {
+            StoreID = storeId;
+            _stockRepository = new StoreRepository();
+        }
+        public float ProductsInStock(int productId)
+        {
+            return _stockRepository.GetProductsQuantityInStock(productId);
+        }
+
+        public void AddNewProduct(int productId, float quantity)
+        {
+            _stockRepository.AddProductToStock(this.StoreID, productId, quantity);
+        }
+
+        public void DeductProductFromStock(int productId, float quantity)
+        {
+            _stockRepository.DeductProductFromStock(this.StoreID, productId, quantity);
+        }
+    }
+}
