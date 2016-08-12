a5e749270d1e5f3fa1e2c62c655d9ac16585d4a5
anick-chy <anickchowdhury@gmail.com>
2016-08-13 05:24:38 +0600

    Purchase Aggregate Commit
    Purchase aggregate created and all others are deleted.

---
+++ PointerPos.Domain/PurchaseAggregate/Model/Item.cs
@@ -0,0 +1,32 @@
+﻿using PointerPos.Domain.PurchaseAggregate.Repositories;
+using PointerPOS.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointerPos.Domain.PurchaseAggregate.Model
+{
+    public class Item : EntityBase<int>
+    {
+        public decimal UnitCost { get; private set; }
+        public int Quantity { get; private set; }
+        private int itemID;
+        private readonly IItemRepository _itemRepository;
+        public Purchase PurchaseBelong { get; private set; }
+        
+        public Item(Purchase purchase, int itemId, decimal unitCost, int quantity, IItemRepository itemRepository)
+        {
+            PurchaseBelong = purchase;
+            itemID = itemId;
+            UnitCost = unitCost;
+            Quantity = quantity;
+            _itemRepository = itemRepository;
+        }
+
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PointerPos.Domain/PurchaseAggregate/Model/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using PointerPOS.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointerPos.Domain.PurchaseAggregate.Model
+{
+    public class Purchase : EntityBase<int>
+    {
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
